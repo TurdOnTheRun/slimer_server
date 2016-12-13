9f1d48d65dd7b0eec604d192dc25d75f9b998e7b
Maximilian Weber <mw2852@columbia.edu>
2016-12-12 21:43:56 -0500

    first

---
+++ slimer/take_screenshot.js
@@ -0,0 +1,108 @@
+var webpage = require('webpage');
+var system = require('system');
+var fs = require('fs');
+
+var time = new Date();
+
+var openPage = function(url, fileLink, type) {
+    var page = webpage.create();
+    var firstShotTimeout;
+    var finalTimeout;
+
+    var FIRST_SHOT_TIME = 2200;
+    var FINAL_SHOT_TIME = 2200;
+
+    var FILE_FORMAT = '.png';
+
+    page.settings.userAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 6_1_4 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10B350 Safari/8536.25';
+    var BROWSER_WIDTH = 295;
+    var BROWSER_HEIGHT = 597;
+    var CLIP_HEIGHT = 1500;
+    var SHOT_QUALITY = 50;
+
+    if(type && type === 'web'){
+        page.settings.userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:10.0) Gecko/20100101 Firefox/46.0';
+        BROWSER_WIDTH = 1366;
+        BROWSER_HEIGHT = 828;
+        CLIP_HEIGHT = 1500;
+    }
+
+    var renderSetup = function(callback){
+        var clipHeight = CLIP_HEIGHT;
+        page.scrollPosition = {
+            top: 0,
+            left: 0
+        };
+        var dimensions = page.evaluate(function(){
+            return [document.body.scrollHeight, document.body.scrollWidth];
+        });
+
+        // console.log((new Date() - time)/1000);
+
+        if(dimensions[0] < CLIP_HEIGHT){
+            clipHeight = dimensions[0];
+        }
+        page.clipRect = { top: 0, left: 0, width: dimensions[1], height: clipHeight };
+
+        if(typeof(callback) === typeof(Function)){
+            callback(clipHeight, dimensions[1]);
+        }
+    };
+
+    var renderWebpage = function(finalRender, callback){
+        renderSetup(function(height, width){
+            page.render(fileLink, { format: 'png', quality: SHOT_QUALITY });
+            system.stdout.write(JSON.stringify({'finalRender': finalRender, 'height': height, 'width': width}));
+            if(typeof(callback) === typeof(Function)){
+                callback();
+            }
+        });
+    };
+
+    var setFirstShotTimeout = function(){
+        firstShotTimeout = setTimeout(function(){
+            renderWebpage(false);
+        }, FIRST_SHOT_TIME);
+    };
+
+    var setFinalTimeout = function(){
+        finalTimeout = setTimeout(function(){
+            renderWebpage(true, function(){ slimer.exit(); });
+        }, FINAL_SHOT_TIME);
+    };
+
+    page.viewportSize = { width: BROWSER_WIDTH, height: BROWSER_HEIGHT };
+
+    page.settings.resourceTimeout = 4000;
+
+    page.onError = function (msg) {
+        system.stderr.writeLine(msg);
+    };
+    page.onConsoleMessage = function(msg, lineNum, sourceId) {
+        system.stderr.writeLine( 'CONSOLE: ' + msg, lineNum, sourceId );
+    };
+
+    setFinalTimeout();
+    // setFirstShotTimeout();
+
+    page.open(url, function(status){
+        if(status !== 'success'){
+            //__ERROR__
+            slimer.exit(1);
+        }
+        else{
+            page.scrollPosition = {
+                top: 500,
+                left: 0
+            };
+        }
+    });
+};
+
+if(system.args.length < 3){
+    system.stderr.write("Failed to run render_webpage_mobile.js\nUsage: slimerjs render_webpage_mobile.js url fileLink (type)");
+    slimer.exit(1);
+}
+else{
+    openPage(system.args[1], system.args[2], system.args[3]);
+}
---
+++ slimer_server.js
@@ -0,0 +1,129 @@
+var args = process.argv.splice(2);
+var net = require('net');
+var child = require('child_process');
+var path = require('path');
+var s3 = require('s3');
+
+var start_slimer = 'start-slimerjs ' + path.join(__dirname, 'slimer', 'take_screenshot.js');
+var start_slimer_OSX = 'slimerjs ' + path.join(__dirname, 'slimer', 'take_screenshot.js');
+var kill_slimer = path.join(__dirname, 'bash_scripts', 'kill-slimerjs');
+
+var parentServerHost;
+var parentServerPort;
+
+var killTimeout;
+var KILL_SLIMER_TIME = 8000;
+
+var socket = new net.Socket();
+
+var S3_BUCKET_NAME = 'siteshots';
+var client = s3.createClient({
+    s3RetryDelay: 200, // this is the default 
+    s3Options: {
+        accessKeyId: 'AKIAIZR6THRT3SL3W7XQ',
+        secretAccessKey: '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',
+    },
+});
+
+var callSlimer = function(elem){
+
+    var slimer = start_slimer_OSX + ' ' + ' \"' + elem.Url + '\"' + ' \"' + path.join(__dirname, elem.fileLink) + '\"';
+    
+    if(elem.type === 'web'){
+        slimer += ' web';
+    }
+
+    var c = child.exec(slimer);
+
+    c.stderr.on('data', function(data){
+        // ___ERROR___
+        console.log(data);
+    });
+
+    c.stdout.on('data', function(data){
+        data = JSON.parse(data);
+        if(data.finalRender){
+            var params = {
+                localFile: path.join(__dirname, elem.fileLink),
+             
+                s3Params: {
+                    Bucket: S3_BUCKET_NAME,
+                    Key: elem.fileLink,
+                    ACL: 'public-read',
+                },
+            };
+            var uploader = client.uploadFile(params);
+            uploader.on('error', function(err) {
+                console.error("unable to upload:", err.stack);
+            });
+            uploader.on('end', function() {
+            });
+        }
+    });
+
+    c.on('exit', function(code){
+        if(code !== 0){
+            console.log('Failed to load ' + elem.Url);
+            //___ERROR___
+        }else{
+            killSlimer();
+        }
+    });
+
+    killTimeout = setTimeout(function(){
+        killSlimer();
+    }, KILL_SLIMER_TIME);
+
+};
+
+var killSlimer = function(){
+
+    var c = child.exec(kill_slimer);
+    c.on('exit', function(code){
+        clearTimeout(killTimeout);
+        checkServer();
+    });
+
+};
+
+var checkServer = function(){
+    sendServerReady();
+};
+
+var resetServer = function(){
+
+};
+
+var sendServerReady = function(){
+    socket.write(JSON.stringify({
+        ready: true
+    }));
+};
+
+socket.on('data', function(data){
+    try{
+        data = JSON.parse(data);
+    } catch(e){
+        return;
+    }
+    if(data.Url && data.fileLink){
+        callSlimer(data);
+    }
+});
+
+
+if(args.length !== 2){
+    console.log('Failed to run slimer_server.js\nUsage: node slimer_server.js parentServerHost parentServerPort');
+    process.exit(1);
+}
+else{
+    parentServerHost = args[0];
+    parentServerPort = args[1];
+
+    socket.connect(parentServerPort, parentServerHost, function(){
+            console.log('Connected to server');
+            sendServerReady();
+    });
+}
+
+
